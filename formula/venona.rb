627a11c915377e350982534ceaea6f5db985f01d
Oleg Sucharevich <olegs@codefresh.io>
2019-01-10 17:53:20 +0200

    initial commit

---
+++ formula/venona.rb
@@ -0,0 +1,15 @@
+class Program < Formula
+    desc "venona --help"
+    homepage "http://docs.codefresh.io"
+    url "https://github.com/codefresh-io/venona/releases/download/v0.6.2/venona_0.6.2_Darwin_x86_64.tar.gz"
+    version "v0.6.2"
+    sha256 "64b8ab8119d5816afa7fe6fe848f0663a3e4b93afdc12afe3fed9fc375c48191"
+  
+    def install
+      bin.install "venona"
+    end
+
+    test do
+        system "#{bin}/venona version"
+      end
+  end
