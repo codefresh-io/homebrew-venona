6a8d4c403e23170f22be81a199ad2a088a716c00
Oleg Sucharevich <olegs@codefresh.io>
2019-01-13 12:35:34 +0200

    Brew formula update for venona version v0.7.0

--- Formula/venona.rb
+++ Formula/venona.rb
@@ -1,9 +1,9 @@
 class Venona < Formula
   desc "venona --help"
   homepage "http://docs.codefresh.io"
-  url "https://github.com/codefresh-io/venona/releases/download/v0.6.5/venona_0.6.5_Darwin_x86_64.tar.gz"
-  version "0.6.5"
-  sha256 "465a1afcf99b852cefd6584897b637d7c1e1e5bec209a928a27285a29f4f5bd8"
+  url "https://github.com/codefresh-io/venona/releases/download/v0.7.0/venona_0.7.0_Darwin_x86_64.tar.gz"
+  version "0.7.0"
+  sha256 "26a29b3dc4e30395f078602baf06d81ae94f827e3a43782d9670269842258399"
 
   def install
     bin.install "venona"
