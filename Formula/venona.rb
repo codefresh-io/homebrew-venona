a7709b6d33cd2af764cddc091cea8cbd16c3990e
Oleg Sucharevich <olegs@codefresh.io>
2019-11-05 13:28:50 +0200

    Brew formula update for venona version v0.28.4

--- Formula/venona.rb
+++ Formula/venona.rb
@@ -2,16 +2,16 @@
 class Venona < Formula
   desc "venona --help"
   homepage "http://docs.codefresh.io"
-  version "0.28.2"
+  version "0.28.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/codefresh-io/venona/releases/download/v0.28.2/venona_0.28.2_Darwin_x86_64.tar.gz"
-    sha256 "801167cfc33f73aac059063c2cac737c16b3c44f038a4b904547511779f0c0c5"
+    url "https://github.com/codefresh-io/venona/releases/download/v0.28.4/venona_0.28.4_Darwin_x86_64.tar.gz"
+    sha256 "062aa29f8e1aea5877d76b58c341548283b29d51171de3e2c9009520a8034340"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/codefresh-io/venona/releases/download/v0.28.2/venona_0.28.2_Linux_x86_64.tar.gz"
-      sha256 "335f668215d1bd5b483ae3139a9236ea16bef87ee0d46e7383bf82969f43692b"
+      url "https://github.com/codefresh-io/venona/releases/download/v0.28.4/venona_0.28.4_Linux_x86_64.tar.gz"
+      sha256 "3c04c66b266babe349ebd483688398bff28f1740741adf224b0aef5fedbfe1ab"
     end
   end
 
